3037ebbb5388f1308e1b185e5cc99ed4ce5100b3
Tower-7 <982155153@qq.com>
2017-01-17 01:16:16 +0800

    submit

--- bootstrap/js/costomize.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('nav .collapse .nav li').mouseover(function(){
-		$(this).addClass('active')
-	})
-	$('nav .collapse .nav li').mouseout(function(){
-		$(this).removeClass('active')
-	})
-})
