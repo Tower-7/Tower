8f3ac68d4710584342aad3848dd58414eb15f963
Tower-7 <982155153@qq.com>
2017-02-06 18:50:03 +0800

    submit

--- project/Web/js/common.js
+++ project/Web/js/common.js
@@ -3,13 +3,29 @@ $(document).ready(function(){
 	getLength()
 })
 var getLength = function(){
-	var lengthTop = $('.product').offset().top - $('.product').scrollTop()
+	var lengthTop = 0
+	var screenHeight = $(window).height();
+	productAnimation(lengthTop,screenHeight)
+	showAnimation(lengthTop,screenHeight)
+}
+
+var productAnimation = function(lengthTop,screenHeight){
 	$(document).scroll(function(){
-		// alert($('.product').scrollTop())
-		if(lengthTop=='750'){
-			// alert(lengthTop)
+		lengthTop = $('.product').offset().top - $(window).scrollTop()-screenHeight/2
+		if(lengthTop <= screenHeight){
+			$('.product .dd li').eq(0).addClass('corporate')
+			$('.product .dd li').eq(1).addClass('shop')
+			$('.product .dd li').eq(2).addClass('education')
 		}
 	})
-	
-		
+}
+var showAnimation = function(lengthTop,screenHeight){
+	$(document).scroll(function(){
+		lengthTop = $('.show').offset().top - $(window).scrollTop()+400
+		if(lengthTop <= screenHeight){
+			$('.show>div').addClass('showScal')
+			$('.show .dots .title').addClass('showMove')
+			$('.news').addClass('newsHeight')
+		}
+	})
 }
